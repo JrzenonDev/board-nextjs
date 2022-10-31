c7a977ed12da2ee92bfbe6db3b7672f1128337e5
JrzenonDev <jrobertoonb@gmail.com>
2022-10-31 02:30:07 -0300

    feat: configure global styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,13 @@
+import Head from 'next/head';
 import styles from '../styles/styles.module.scss';
 
 export default function Home() {
   return (
-    <div>
+    <>
+      <Head>
+        <title>Board - Organizando suas tarefas</title>
+      </Head>
       <h1 className={styles.title}>Primeira aula nexjs</h1>
-    </div>
+    </>
   );
 }
