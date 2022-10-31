e4871715229d2b655c052b5b222a2b5655861e70
JrzenonDev <jrobertoonb@gmail.com>
2022-10-31 01:27:56 -0300

    add and configure sass in project

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
+import styles from '../styles/styles.module.scss';
+
 export default function Home() {
   return (
     <div>
-      <h1>Primeira aula nexjs</h1>
+      <h1 className={styles.title}>Primeira aula nexjs</h1>
     </div>
   );
 }
