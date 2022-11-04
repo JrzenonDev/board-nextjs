ef14c8ffa1d72f6c464c35acd08398074fab37e5
JrzenonDev <jrobertoonb@gmail.com>
2022-11-04 02:29:12 -0300

    refect: downgrade in version next-auth and update code using this version

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import { SessionProvider as NextAuthProvider } from 'next-auth/react';
+import { Provider as NextAuthProvider } from 'next-auth/client';
 import { AppProps } from 'next/app';
 import { Header } from '../components/Header';
 import '../styles/global.scss';
