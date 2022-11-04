b0d10807161f86806e0cd99f47f3b5ddac9e3de6
JrzenonDev <jrobertoonb@gmail.com>
2022-11-03 21:31:56 -0300

    feat: create login/logout with nextauth

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,26 +1,27 @@
+import { signIn, signOut, useSession } from 'next-auth/react';
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
 import styles from './styles.module.scss';
 
 export default function SignInButton() {
 
-  const session = false;
+  const { data: session } = useSession();
 
   return session ? (
     <button
       type="button"
       className={styles.signInButton}
-      onClick={() => {}}
+      onClick={() => signOut() }
     >
-      <img src="https://sujeitoprogramador.com/steve.png" alt="Foto do usuário" />
-      Olá José
+      <img src={session.user.image} alt="Foto do usuário" />
+      Olá {session.user.name}
       <FiX color='#737380' className={styles.closeIcon} />
     </button>
   ) : (
     <button
       type="button"
       className={styles.signInButton}
-      onClick={() => {}}
+      onClick={() => signIn('github') }
     >
       <FaGithub color='#ffb800' />
       Entrar com github
