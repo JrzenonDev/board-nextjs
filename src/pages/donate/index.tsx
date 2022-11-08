1613e4befa46a5b89a16e1b6f75cbc04c1267e0d
JrzenonDev <jrobertoonb@gmail.com>
2022-11-08 01:36:04 -0300

    feat: init page donate with styles in layout

---
+++ src/pages/donate/index.tsx
@@ -0,0 +1,24 @@
+import Head from 'next/head';
+import styles from './styles.module.scss';
+
+export default function Donate() {
+  return (
+    <>
+      <Head>
+        <title>Apoie a página Board a ficar online</title>
+      </Head>
+      <main className={styles.container}>
+        <img src='/images/rocket.svg' alt='Seja apoiador' />
+
+        <div className={styles.vip}>
+          <img src='https://avatars.githubusercontent.com/u/11636904?v=4' alt='Seja apoiador' />
+          <span>Parabéns você é um apoiador.</span>
+        </div>
+
+        <h1>Seja um apoiador desse projeto 🏆</h1>
+        <h3>Contribua com apenas <span>R$ 1,00</span></h3>
+        <strong>Apareça na nossa home, tenha funcionalidades exclusivas.</strong>
+      </main>
+    </>
+  )
+}
