46fadf24d56c8d3677f09c7cf06d04287a9800a9
JrzenonDev <jrobertoonb@gmail.com>
2022-11-10 23:45:17 -0300

    feat: configure purchase method paypal in donate page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,23 @@
+import { PayPalScriptProvider } from '@paypal/react-paypal-js';
 import { Provider as NextAuthProvider } from 'next-auth/client';
 import { AppProps } from 'next/app';
 import { Header } from '../components/Header';
+
 import '../styles/global.scss';
+
+const initialOptions = {
+  'client-id': 'AXIM_FXxxdmOHs8EEvrE2Xoh3FFiwHHAz3fQ9UUk5nslemayJp9TBNlMs93bAO05EN2XmZhmR7owQLSy',
+  currency: 'BRL',
+  intent: 'capture'
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <NextAuthProvider session={pageProps.session}>
-      <Header />
-      <Component {...pageProps} />
+      <PayPalScriptProvider options={initialOptions}>
+        <Header />
+        <Component {...pageProps} />
+      </PayPalScriptProvider>
     </NextAuthProvider>
   );
 }
--- src/pages/donate/index.tsx
+++ src/pages/donate/index.tsx
@@ -1,3 +1,4 @@
+import { PayPalButtons } from '@paypal/react-paypal-js';
 import { GetServerSideProps } from 'next';
 import { getSession } from 'next-auth/client';
 import Head from 'next/head';
@@ -28,6 +29,23 @@ export default function Donate({ user }: DonateProps) {
         <h1>Seja um apoiador desse projeto 🏆</h1>
         <h3>Contribua com apenas <span>R$ 1,00</span></h3>
         <strong>Apareça na nossa home, tenha funcionalidades exclusivas.</strong>
+
+        <PayPalButtons
+          createOrder={(data, actions) => {
+            return actions.order.create({
+              purchase_units: [{
+                amount: {
+                  value: '1'
+                }
+              }]
+            })
+          }}
+          onApprove={(data, actions) => {
+            return actions.order.capture().then(function(datails) {
+              console.log('compra aprovada: ', datails.payer?.name.given_name);
+            })
+          }}
+        />
       </main>
     </>
   )
